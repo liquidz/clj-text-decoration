2e2d781db99fa0d076408fd9aaf6382bc2452aca
Jakub Holy <jakub.holy@telia.no>
2020-11-02 09:59:30 +0100

    Clarify the library's purpose
    because "text decoration" was not clear enough to me.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-text-decoration "0.0.3"
-  :description  "Text decoration library"
+(defproject clj-text-decoration "0.0.4"
+  :description  "Text decoration library for color and style in the terminal"
   :url          "http://liquidz.github.com"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
